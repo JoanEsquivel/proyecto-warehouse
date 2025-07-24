0045501e1e49c6f17b0eefd20c6e48658dbb543a
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2025-07-24 15:44:32 -0600

    feat: creacion del modelo SA

--- 7. Creación del Modelo SA.sql
+++ 7. Creación del Modelo SA.sql
@@ -1,38 +1,52 @@
 --------------------------------------------------------------------------------
--- Creación del Modelo Relacional.
+-- Creaciï¿½n del Modelo Staging Area.
 -- Se conecta con el usuario creado para el modelo ER.
 --------------------------------------------------------------------------------
-CREATE TABLE SA_TIPO_CUENTA (
-   TCT_ID          VARCHAR2(4000),
-   TCT_DESCRIPCION VARCHAR2(4000)
+
+-- Es una copia del modelo ER, quitarle las restricciones y agregarle un prefijo SA_
+-- Ademas los tipos de datos de las columnas deben ser VARCHAR2
+
+
+DROP TABLE SA_ORDEN_COMPRA;
+DROP TABLE SA_PRODUCTO;
+DROP TABLE SA_CLIENTE;
+DROP TABLE SA_TIPO_PRODUCTO;
+DROP TABLE SA_TIPO_ENVIO;
+
+commit;
+
+CREATE TABLE SA_TIPO_ENVIO (
+   TPE_ID                    VARCHAR2(4000),
+   TPE_DESCRIPCION           VARCHAR2(4000), 
+   TPE_ESTADO                VARCHAR2(4000), 
+   TPE_REQUIERE_CONFIRMACION VARCHAR2(4000)
 );
 
-CREATE TABLE SA_TIPO_CLIENTE (
-   TCL_ID          VARCHAR2(4000),
-   TCL_DESCRIPCION VARCHAR2(4000)
-);
-
-CREATE TABLE SA_ESTADO_CLIENTE (
-   ECT_ID          VARCHAR2(4000),
-   ECT_DESCRIPCION VARCHAR2(4000)
+CREATE TABLE SA_TIPO_PRODUCTO (
+   TPD_ID                   VARCHAR2(4000),
+   TPD_DESCRIPCION          VARCHAR2(4000)
 );
 
 CREATE TABLE SA_CLIENTE (
-   CTE_ID     VARCHAR2(4000),
-   CTE_TCL_ID VARCHAR2(4000),
-   CTE_ECT_ID VARCHAR2(4000)
+   CTE_ID                   VARCHAR2(4000),
+   CTE_NOMBRE               VARCHAR2(4000)
 );
 
-CREATE TABLE SA_CUENTA (
-   CTA_ID            VARCHAR2(4000),
-   CTA_CTE_ID        VARCHAR2(4000),
-   CTA_TCT_ID        VARCHAR2(4000),
-   CTA_NUMERO_CUENTA VARCHAR2(4000)
+CREATE TABLE SA_PRODUCTO (
+   PRD_ID                  VARCHAR2(4000),
+   PRD_TPD_ID              VARCHAR2(4000),
+   PRD_NOMBRE              VARCHAR2(4000),
+   PRD_CANTIDAD            VARCHAR2(4000),
+   PRD_COSTO_UNITARIO      VARCHAR2(4000)
+   
 );
 
-CREATE TABLE SA_MOVIMIENTO (
-   MVM_ID     VARCHAR2(4000),
-   MVM_CTA_ID VARCHAR2(4000),
-   MVM_FECHA  VARCHAR2(4000),
-   MVM_MONTO  VARCHAR2(4000)
+CREATE TABLE SA_ORDEN_COMPRA (
+   OCP_ID                 VARCHAR2(4000),
+   OCP_PRD_ID             VARCHAR2(4000),
+   OCP_CTE_ID             VARCHAR2(4000),
+   OCP_TPE_ID             VARCHAR2(4000),
+   OCP_FECHA              VARCHAR2(4000)
 );
+
+commit;
